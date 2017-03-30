4cda00d6724f0b73a6b9de5625bf6dd5599d2053
Jacob Haip <jhaip@mit.edu>
2017-03-29 21:06:52 -0400

    Photon firmware flash of blink.ino

--- photon/blink.ino
+++ photon/blink.ino
@@ -1,6 +1,6 @@
 int led1 = D0; // Instead of writing D0 over and over again, we'll write led1
 int led2 = D7; // Instead of writing D7 over and over again, we'll write led2
-unsigned long delay = 200;
+int delay_ms = 200;
 
 void setup() {
   pinMode(led1, OUTPUT);
@@ -11,12 +11,12 @@ void loop() {
   digitalWrite(led1, HIGH);
   digitalWrite(led2, HIGH);
 
-  delay(delay);
+  delay(delay_ms);
 
   digitalWrite(led1, LOW);
   digitalWrite(led2, LOW);
 
-  delay(delay);
+  delay(delay_ms);
 }
 
 
